35e9135ce22ba3ec5fdefc2d988fe2ba3e87c262
Benoît Canet <benoit.canet@nodalink.com>
2014-12-01 16:01:21 +0100

    collect: Add a collect brick for pkts flow testing
    The collect brick only purpose is to collect packets then serve them to
    the tests.
    
    Signed-off-by: Benoît Canet <benoit.canet@nodalink.com>

---
+++ bricks/collect-brick.c
@@ -0,0 +1,83 @@
+/* Copyright 2014 Nodalink EURL
+ *
+ * This file is part of Butterfly.
+ *
+ * Butterfly is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published
+ * by the Free Software Foundation.
+ *
+ * Butterfly is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Butterfly.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <string.h>
+#include <ccan/build_assert/build_assert.h>
+
+#include "switch-config.h"
+#include "bricks/brick.h"
+
+struct collect_state {
+	struct brick brick;
+	/* arrays of collected incoming packets */
+	struct rte_mbuf *pkts[MAX_SIDE][MAX_PKTS_BURST];
+	uint64_t pkts_mask[MAX_SIDE];
+};
+
+static void collect_burst(struct brick *brick, enum side side,
+			  struct rte_mbuf **pkts, uint16_t nb,
+			  uint64_t pkts_mask)
+{
+	struct collect_state *state =
+		brick_get_state(brick, struct collect_state);
+
+	BUILD_ASSERT(MAX_PKTS_BURST == 64);
+	g_assert(nb <= MAX_PKTS_BURST);
+
+	state->pkts_mask[side] = pkts_mask;
+	/* We made sure nb <= MAX_PKTS_BURST */
+	/* Flawfinder: ignore */
+	memcpy(state->pkts[side], pkts, nb * sizeof(struct rte_mbuf *));
+}
+
+static struct rte_mbuf **collect_burst_get(struct brick *brick, enum side side,
+					   uint64_t *pkts_mask)
+{
+	struct collect_state *state =
+		brick_get_state(brick, struct collect_state);
+
+	if (!state->pkts_mask[side]) {
+		*pkts_mask = 0;
+		return NULL;
+	}
+
+	*pkts_mask = state->pkts_mask[side];
+	return state->pkts[side];
+}
+
+static int collect_init(struct brick *brick)
+{
+	brick->burst = collect_burst;
+	brick->burst_get = collect_burst_get;
+	brick_set_max_edges(brick, 1, 1);
+
+	return 1;
+}
+
+static struct brick_ops collect_ops = {
+	.name		= "collect",
+	.state_size	= sizeof(struct collect_state),
+
+	.init		= collect_init,
+
+	.west_link	= brick_generic_west_link,
+	.east_link	= brick_generic_east_link,
+	.unlink		= brick_generic_unlink,
+};
+
+brick_register(struct collect_state, &collect_ops);
---
+++ include/switch-config.h
@@ -0,0 +1,7 @@
+#ifndef _SWITCH_CONFIG_H_
+#define _SWITCH_CONFIG_H_
+
+/* do not change this */
+#define MAX_PKTS_BURST	64
+
+#endif
