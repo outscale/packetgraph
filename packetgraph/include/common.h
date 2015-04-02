9c5bb395d4071c96bc1f8419ab6e6db719b9170d
Matthias Gatto <matthias.gatto@outscale.com>
2015-04-02 19:45:34 +0200

    common: add licence.
    This file has been made by benoit in 2014 under the name of switch-config.h,
    but he forgot the licence.
    The problem is now fix
    
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- packetgraph/include/common.h
+++ packetgraph/include/common.h
@@ -1,3 +1,20 @@
+/* Copyright 2014 Nodalink EURL
+ *
+ * This file is part of Butterfly.
+ *
+ * Butterfly is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published
+ * by the Free Software Foundation.
+ *
+ * Butterfly is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Butterfly.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
