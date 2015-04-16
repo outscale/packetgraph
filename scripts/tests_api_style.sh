cba334f8f9b3614129ecb376b2b607ecad895334
Jérôme Jutteau <jerome.jutteau@outscale.com>
2015-04-16 12:47:53 +0200

    api/server: create model
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- scripts/tests_api_style.sh
+++ scripts/tests_api_style.sh
@@ -20,7 +20,10 @@ sources="$BUTTERFLY_ROOT/api/client/client.cc \
 $BUTTERFLY_ROOT/api/server/app.cc \
 $BUTTERFLY_ROOT/api/server/app.h \
 $BUTTERFLY_ROOT/api/server/server.cc \
-$BUTTERFLY_ROOT/api/server/server.h"
+$BUTTERFLY_ROOT/api/server/server.h \
+$BUTTERFLY_ROOT/api/server/model.cc \
+$BUTTERFLY_ROOT/api/server/model.h"
+
 
 $BUTTERFLY_ROOT/scripts/cpplint.py --filter=-build/c++11 --root=$BUTTERFLY_ROOT $sources
 if [ $? != 0 ]; then
