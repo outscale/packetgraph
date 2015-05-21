outscale/butterfly

utils: add lifecycle.
Add an api use to start and stop packetgraph

Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

packetgraph/include/utils/lifecycle.h
@@ -1,0 +1,29 @@
+/* Copyright 2015 Outscale SAS
+ *
+ * This file is part of Butterfly.
+ *
+ * Butterfly is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published
+ * by the Free Software Foundation.
+ *
+ * Butterfly is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Butterfly.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _LIFECYCLE_H_
+#define _LIFECYCLE_H_
+
+#include "utils/errors.h"
+
+int packetgraph_start(int argc, char **argv,
+		      const char *base_dir,
+		      struct switch_error **errp);
+
+void packetgraph_stop(void);
+
+#endif

outscale/packetgraph

Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

packetgraph/include/utils/lifecycle.h
@@ -1,0 +1,29 @@
+/* Copyright 2015 Outscale SAS
+ *
+ * This file is part of Butterfly.
+ *
+ * Butterfly is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published
+ * by the Free Software Foundation.
+ *
+ * Butterfly is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Butterfly.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _UTILS_LIFECYCLE_H_
+#define _UTILS_LIFECYCLE_H_
+
+#include "utils/errors.h"
+
+int packetgraph_start(int argc, char **argv,
+		      const char *base_dir,
+		      struct switch_error **errp);
+
+void packetgraph_stop(void);
+
+#endif