523e960ee00ca2aa5f2a0dc2932bac0b5f5edff6
Jérôme Jutteau <jerome.jutteau@outscale.com>
2015-07-26 03:36:04 +0200

    add brick-print to show packets informations
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

---
+++ brick-print/src/printer.h
@@ -0,0 +1,41 @@
+/* Copyright 2015 Outscale SAS
+ *
+ * This file is part of Butterfly.
+ *
+ * Butterfly is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published
+ * by the Free Software Foundation.
+ *
+ * Butterfly is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Butterfly.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _BRICKS_BRICK_PRINTER_H_
+#define _BRICKS_BRICK_PRINTER_H_
+
+#include <glib.h>
+
+/**
+ * Inspect a packet and write a short summary
+ *
+ * @param	data packet's data.
+ * @param	size packet's size.
+ * @param	output where to write informations
+ */
+void print_summary(void *data, size_t size, FILE *ouput);
+
+/**
+ * Inspect a packet and write it's raw data (hex)
+ *
+ * @param	data packet's data.
+ * @param	size packet's size.
+ * @param	output where to write informations
+ */
+void print_raw(void *data, size_t size, FILE *output);
+
+#endif  /* _BRICKS_BRICK_PRINTER_H_ */
---
+++ brick-print/tests/tests.c
@@ -0,0 +1,149 @@
+/* Copyright 2015 Outscale SAS
+ *
+ * This file is part of Butterfly.
+ *
+ * Butterfly is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published
+ * by the Free Software Foundation.
+ *
+ * Butterfly is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Butterfly.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <net/ethernet.h>
+#include <netinet/ether.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+
+#include <packetgraph/packetgraph.h>
+#include <packetgraph/print.h>
+#include <packetgraph/brick.h>
+#include <packetgraph/packets.h>
+#include <packetgraph/utils/mempool.h>
+#include <packetgraph/utils/bitmask.h>
+#include <packetgraph/collect.h>
+#include <packetgraph/packetsgen.h>
+
+#define NB_PKTS 3
+
+static struct rte_mbuf *build_ip_packet(void)
+{
+	struct rte_mempool *mp = get_mempool();
+	struct rte_mbuf *pkt = rte_pktmbuf_alloc(mp);
+	uint16_t len = sizeof(struct ether_header) + sizeof(struct ip) + 6;
+	struct ether_header *eth;
+	struct ip *ip;
+	uint8_t *payload_ip;
+
+	pkt->pkt_len = len;
+	pkt->data_len = len;
+	pkt->nb_segs = 1;
+	pkt->next = NULL;
+
+	/* ethernet header */
+	eth = rte_pktmbuf_mtod(pkt, struct ether_header*);
+	memset(eth, 0, len);
+	eth->ether_type = htobe16(ETHERTYPE_IP);
+
+	/* ipv4 header */
+	ip = (struct ip *)(eth + 1);
+	ip->ip_v = IPVERSION;
+	ip->ip_hl = sizeof(struct ip) >> 2;
+	ip->ip_off = 0;
+	ip->ip_ttl = 64;
+
+	/* FEAR ! this is CHAOS ! */
+	ip->ip_p = 16;
+	ip->ip_len = htons(sizeof(struct ip) + 1);
+	ip->ip_src.s_addr = inet_addr("10.0.42.1");
+	ip->ip_dst.s_addr = inet_addr("10.0.42.2");
+
+	/* write some data */
+	payload_ip = (uint8_t *)(ip + 1);
+	payload_ip[0] = 0x40;
+	payload_ip[1] = 0x41;
+	payload_ip[2] = 0x42;
+	payload_ip[3] = 0x43;
+	payload_ip[4] = 0x44;
+	payload_ip[5] = 0x45;
+	return pkt;
+}
+
+static void build_packets(struct rte_mbuf *packets[NB_PKTS])
+{
+	int i;
+
+	for (i = 0; i < NB_PKTS; i++)
+		packets[i] = build_ip_packet();
+}
+
+static void test_print_simple(void)
+{
+	int i;
+	struct brick *gen, *print, *col;
+	struct switch_error *error = NULL;
+	struct rte_mbuf *packets[NB_PKTS];
+	struct rte_mbuf **pkts;
+	uint64_t pkts_mask;
+
+	build_packets(packets);
+	gen = packetsgen_new("gen", 1, 1, EAST_SIDE, packets, NB_PKTS, &error);
+	g_assert(!error);
+	print = print_new("My print", 1, 1, NULL, PRINT_FLAG_MAX, &error);
+	g_assert(!error);
+	col = collect_new("col", 1, 1, &error);
+	g_assert(!error);
+
+	brick_link(gen, print, &error);
+	g_assert(!error);
+	brick_link(print, col, &error);
+	g_assert(!error);
+
+	brick_burst_to_east(gen, 0, packets, NB_PKTS, mask_firsts(NB_PKTS),
+			    &error);
+	g_assert(!error);
+
+	pkts = brick_west_burst_get(col, &pkts_mask, &error);
+	g_assert(!error);
+	packets_free(pkts, mask_firsts(NB_PKTS));
+
+	brick_unlink(gen, &error);
+	g_assert(!error);
+	brick_unlink(print, &error);
+	g_assert(!error);
+	brick_unlink(col, &error);
+	g_assert(!error);
+	brick_destroy(gen);
+	brick_destroy(print);
+	brick_destroy(col);
+	for (i = 0; i < NB_PKTS; i++)
+		rte_pktmbuf_free(packets[i]);
+}
+
+int main(int argc, char **argv)
+{
+	struct switch_error *error;
+	int r;
+
+	/* tests in the same order as the header function declarations */
+	g_test_init(&argc, &argv, NULL);
+
+	/* initialize packetgraph */
+	packetgraph_start(argc, argv, &error);
+	g_assert(!error);
+
+	g_test_add_func("/brick/print/simple", test_print_simple);
+
+	r = g_test_run();
+
+	packetgraph_stop();
+	return r;
+}
