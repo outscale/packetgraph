5ce78950f72cc79adaf04d72eab268a0a887732d
Jérôme Jutteau <jerome.jutteau@outscale.com>
2015-08-20 09:28:50 +0200

    firewall: fix define for DEFAULT_MBUF_DATA_SIZE
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- core/src/utils/mempool.c
+++ core/src/utils/mempool.c
@@ -22,7 +22,7 @@
 #include <packetgraph/utils/mempool.h>
 
 #define NUM_MBUFS 8191
-#define MBUF_SIZE (RTE_MBUF_DEFAULT_BUF_SIZE +  sizeof(struct rte_mbuf))
+#define MBUF_SIZE (2048 + sizeof(struct rte_mbuf))
 #define MBUF_CACHE_SIZE 250
 
 static struct rte_mempool *mp;
