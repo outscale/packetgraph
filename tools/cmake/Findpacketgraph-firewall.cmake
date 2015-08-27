3b6e6062cf591f340143925f7a594fb30cc64cc0
Matthias Gatto <matthias.gatto@outscale.com>
2015-08-27 11:00:28 +0200

    cmake: add Findpacketgraph-vhost/nic/vtep/firewall
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ tools/cmake/Findpacketgraph-firewall.cmake
@@ -0,0 +1,44 @@
+# Finds packetgraph-firewall includes and library
+# Defines:
+#  LIBPACKETGRAPH-FIREWALL_INCLUDE_DIR
+#  LIBPACKETGRAPH-FIREWALL_LIBRARY
+#  LIBPACKETGRAPH-FIREWALL_FOUND
+
+# Check if user has set a env variable to get it's own build of firewall
+# If variable is not found, let's get packetgraph-firewall in the system.
+if ("${PG_USER_FIREWALL_BUILD}" STREQUAL "")
+  set (PG_USER_FIREWALL_BUILD $ENV{PG_FIREWALL})
+endif()
+if ("${PG_USER_FIREWALL_BUILD}" STREQUAL "")
+	message ("PG_FIREWALL global variable not set, searching packetgraph-firewall on the system ...")
+
+	find_path(LIBPACKETGRAPH-FIREWALL_ROOT_DIR
+		NAMES include/packetgraph/firewall.h
+	)
+
+	find_path(LIBPACKETGRAPH-FIREWALL_INCLUDE_DIR
+			NAMES firewall.h
+			HINTS ${LIBPACKETGRAPH-FIREWALL_ROOT_DIR}/include/packetgraph
+	)
+
+	set(LIBPACKETGRAPH-FIREWALL_NAME packetgraph-firewall)
+
+	find_library(LIBPACKETGRAPH-FIREWALL_LIBRARY
+			NAMES ${LIBPACKETGRAPH-FIREWALL_NAME}
+			HINTS ${LIBPACKETGRAPH-FIREWALL_ROOT_DIR}/lib
+	)
+
+	include(FindPackageHandleStandardArgs)
+	find_package_handle_standard_args(libpacketgraph-firewall REQUIRED_VARS
+			LIBPACKETGRAPH-FIREWALL_LIBRARY
+			LIBPACKETGRAPH-FIREWALL_INCLUDE_DIR)
+	mark_as_advanced(
+			LIBPACKETGRAPH-FIREWALL_ROOT_DIR
+			LIBPACKETGRAPH-FIREWALL_LIBRARY
+			LIBPACKETGRAPH-FIREWALL_INCLUDE_DIR
+	)
+else()
+	set(LIBPACKETGRAPH-FIREWALL_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/../firewall)
+	set(LIBPACKETGRAPH-FIREWALL_LIBRARY "${PG_USER_FIREWALL_BUILD}/libpacketgraph-firewall.a")
+	set(LIBPACKETGRAPH-FIREWALL_FOUND 1)
+endif()
