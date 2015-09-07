76187a156bb627859807d066c8283bf25a812d7c
Matthias Gatto <matthias.gatto@outscale.com>
2015-09-07 12:06:06 +0200

    cmake: add Findpacketgraph-print.cmake
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ tools/cmake/Findpacketgraph-print.cmake
@@ -0,0 +1,44 @@
+# Finds packetgraph-print includes and library
+# Defines:
+#  LIBPACKETGRAPH-PRINT_INCLUDE_DIR
+#  LIBPACKETGRAPH-PRINT_LIBRARY
+#  LIBPACKETGRAPH-PRINT_FOUND
+
+# Check if user has set a env variable to get it's own build of print
+# If variable is not found, let's get packetgraph-print in the system.
+if ("${PG_USER_PRINT_BUILD}" STREQUAL "")
+  set (PG_USER_PRINT_BUILD $ENV{PG_PRINT})
+endif()
+if ("${PG_USER_PRINT_BUILD}" STREQUAL "")
+	message ("PG_PRINT global variable not set, searching packetgraph-print on the system ...")
+
+	find_path(LIBPACKETGRAPH-PRINT_ROOT_DIR
+		NAMES include/packetgraph/print.h
+	)
+
+	find_path(LIBPACKETGRAPH-PRINT_INCLUDE_DIR
+			NAMES print.h
+			HINTS ${LIBPACKETGRAPH-PRINT_ROOT_DIR}/include/packetgraph
+	)
+
+	set(LIBPACKETGRAPH-PRINT_NAME packetgraph-print)
+
+	find_library(LIBPACKETGRAPH-PRINT_LIBRARY
+			NAMES ${LIBPACKETGRAPH-PRINT_NAME}
+			HINTS ${LIBPACKETGRAPH-PRINT_ROOT_DIR}/lib
+	)
+
+	include(FindPackageHandleStandardArgs)
+	find_package_handle_standard_args(libpacketgraph-print REQUIRED_VARS
+			LIBPACKETGRAPH-PRINT_LIBRARY
+			LIBPACKETGRAPH-PRINT_INCLUDE_DIR)
+	mark_as_advanced(
+			LIBPACKETGRAPH-PRINT_ROOT_DIR
+			LIBPACKETGRAPH-PRINT_LIBRARY
+			LIBPACKETGRAPH-PRINT_INCLUDE_DIR
+	)
+else()
+	set(LIBPACKETGRAPH-PRINT_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/../print)
+	set(LIBPACKETGRAPH-PRINT_LIBRARY "${PG_USER_PRINT_BUILD}/libpacketgraph-print.a")
+	set(LIBPACKETGRAPH-PRINT_FOUND 1)
+endif()
