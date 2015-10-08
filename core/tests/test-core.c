c565e7ec65caaa5365ccb3359de8e981e27da1aa
Jérôme Jutteau <jerome.jutteau@outscale.com>
2015-10-08 11:16:32 +0200

    core/test: add a test on link/relink bricks
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- core/tests/test-core.c
+++ core/tests/test-core.c
@@ -18,6 +18,7 @@
 #include <glib.h>
 
 #include <packetgraph/brick.h>
+#include <packetgraph/nop.h>
 #include <packetgraph/utils/config.h>
 #include "tests.h"
 
@@ -220,6 +221,33 @@ static void test_brick_core_multiple_link(void)
 	pg_brick_config_free(config);
 }
 
+static int test_side_sanity_check(struct pg_brick_side *side)
+{
+	uint16_t j, count = 0;
+
+	for (j = 0; j < side->max; j++) {
+		if (side->edges[j].link)
+			count++;
+	}
+	g_assert(count == side->nb);
+	return count;
+}
+
+
+static void test_brick_sanity_check(struct pg_brick *brick)
+{
+	test_side_sanity_check(&brick->sides[WEST_SIDE]);
+	test_side_sanity_check(&brick->sides[EAST_SIDE]);
+}
+
+static void test_brick_sanity_check_expected(struct pg_brick *brick,
+					     int west, int east)
+{
+	g_assert(test_side_sanity_check(&brick->sides[WEST_SIDE]) == west);
+	g_assert(test_side_sanity_check(&brick->sides[EAST_SIDE]) == east);
+}
+
+
 static void test_brick_core_verify_multiple_link(void)
 {
 	struct pg_brick *west_brick, *middle_brick, *east_brick;
@@ -245,6 +273,11 @@ static void test_brick_core_verify_multiple_link(void)
 	g_assert(!error);
 	pg_brick_link(middle_brick, east_brick, &error);
 	g_assert(!error);
+
+	/* sanity checks */
+	test_brick_sanity_check(west_brick);
+	test_brick_sanity_check(middle_brick);
+	test_brick_sanity_check(east_brick);
 
 	/* check the link count */
 	links_count = pg_brick_links_count_get(west_brick, west_brick, &error);
@@ -280,6 +313,11 @@ static void test_brick_core_verify_multiple_link(void)
 	/* unlink the west brick */
 	pg_brick_unlink(west_brick, &error);
 
+	/* sanity checks */
+	test_brick_sanity_check(west_brick);
+	test_brick_sanity_check(middle_brick);
+	test_brick_sanity_check(east_brick);
+
 	/* check again */
 	links_count = pg_brick_links_count_get(west_brick, west_brick, &error);
 	g_assert(!error);
@@ -314,6 +352,11 @@ static void test_brick_core_verify_multiple_link(void)
 	/* unlink the east brick */
 	pg_brick_unlink(east_brick, &error);
 	g_assert(!error);
+
+	/* sanity checks */
+	test_brick_sanity_check(west_brick);
+	test_brick_sanity_check(middle_brick);
+	test_brick_sanity_check(east_brick);
 
 	/* check again */
 	links_count = pg_brick_links_count_get(west_brick, west_brick, &error);
@@ -357,6 +400,61 @@ static void test_brick_core_verify_multiple_link(void)
 	pg_brick_config_free(config);
 }
 
+static void test_brick_core_verify_re_link(void)
+{
+	struct pg_error *e = NULL;
+	struct pg_brick *v = pg_nop_new("v", 1, 1, &e);
+	g_assert(!e);
+	struct pg_brick *f = pg_nop_new("f", 1, 1, &e);
+	g_assert(!e);
+	struct pg_brick *a = pg_nop_new("a", 1, 1, &e);
+	g_assert(!e);
+	struct pg_brick *s = pg_nop_new("s", 1, 1, &e);
+	g_assert(!e);
+
+	/* Initial state: v -- f -- a */
+	pg_brick_chained_links(&e, v, f, a);
+	g_assert(!e);
+	test_brick_sanity_check_expected(v, 0, 1);
+	test_brick_sanity_check_expected(f, 1, 1);
+	test_brick_sanity_check_expected(a, 1, 0);
+
+	/* Unlink f */
+	pg_brick_unlink(f, &e);
+	g_assert(!e);
+	test_brick_sanity_check_expected(v, 0, 0);
+	test_brick_sanity_check_expected(f, 0, 0);
+	test_brick_sanity_check_expected(a, 0, 0);
+
+	/* Link v and s */
+	pg_brick_link(v, s, &e);
+	g_assert(!e);
+	test_brick_sanity_check_expected(v, 0, 1);
+	test_brick_sanity_check_expected(s, 1, 0);
+	test_brick_sanity_check_expected(f, 0, 0);
+	test_brick_sanity_check_expected(a, 0, 0);
+
+	/* link the rest to have v -- s -- f -- a */
+	pg_brick_link(s, f, &e);
+	g_assert(!e);
+	test_brick_sanity_check_expected(v, 0, 1);
+	test_brick_sanity_check_expected(s, 1, 1);
+	test_brick_sanity_check_expected(f, 1, 0);
+	test_brick_sanity_check_expected(a, 0, 0);
+
+	pg_brick_link(f, a, &e);
+	g_assert(!e);
+	test_brick_sanity_check_expected(v, 0, 1);
+	test_brick_sanity_check_expected(s, 1, 1);
+	test_brick_sanity_check_expected(f, 1, 1);
+	test_brick_sanity_check_expected(a, 1, 0);
+
+	pg_brick_destroy(a);
+	pg_brick_destroy(v);
+	pg_brick_destroy(f);
+	pg_brick_destroy(s);
+}
+
 void test_brick_core(void)
 {
 	/* tests in the same order as the header function declarations */
@@ -368,5 +466,8 @@ void test_brick_core(void)
 			test_brick_core_multiple_link);
 	g_test_add_func("/brick/core/verify/multiple-link",
 			test_brick_core_verify_multiple_link);
+	g_test_add_func("/brick/core/verify/re-link",
+			test_brick_core_verify_re_link);
+
 
 }
