4eb288007126a4f8c7ffe71555c6977a05e23047
Jerome Jutteau <jerome.jutteau@outscale.com>
2015-12-01 15:53:56 +0100

    vhost: use callback to push bursted packets count
    Signed-off-by: Jerome Jutteau <jerome.jutteau@outscale.com>

--- vhost/src/vhost.c
+++ vhost/src/vhost.c
@@ -91,6 +91,8 @@ static int vhost_burst(struct pg_brick *brick, enum pg_side from,
 	struct pg_vhost_state *state;
 	struct virtio_net *virtio_net;
 	uint16_t pkts_count;
+	uint16_t bursted_pkts;
+	struct pg_brick_side *side;
 
 	state = pg_brick_get_state(brick, struct pg_vhost_state);
 
@@ -115,10 +117,17 @@ static int vhost_burst(struct pg_brick *brick, enum pg_side from,
 
 	pg_packets_incref(pkts, pkts_mask);
 	pkts_count = pg_packets_pack(state->out, pkts, pkts_mask);
-	rte_vhost_enqueue_burst(virtio_net, VIRTIO_RXQ, state->out,
-					 pkts_count);
-
+	bursted_pkts = rte_vhost_enqueue_burst(virtio_net,
+					       VIRTIO_RXQ,
+					       state->out,
+					       pkts_count);
 	rcu_read_unlock();
+
+	side = &brick->sides[pg_flip_side(from)];
+	if (side->burst_count_cb != NULL)
+		side->burst_count_cb(side->burst_count_private_data,
+				     bursted_pkts);
+
 	pg_packets_free(state->out, pg_mask_firsts(pkts_count));
 
 	return 1;
