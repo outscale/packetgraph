c92d788e046e4ed33d8de727d1632628072b1435
Jerome Jutteau <jerome.jutteau@outscale.com>
2016-02-26 10:22:27 +0100

    diode: fix uninitialized error
    Signed-off-by: Jerome Jutteau <jerome.jutteau@outscale.com>

--- diode/tests/tests.c
+++ diode/tests/tests.c
@@ -183,7 +183,7 @@ static void test_diode(void)
 
 int main(int argc, char **argv)
 {
-	struct pg_error *error;
+	struct pg_error *error = NULL;
 	int r;
 
 	/* tests in the same order as the header function declarations */
