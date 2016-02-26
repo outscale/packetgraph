347f819465a6fae55e80a233a839ef4f0273c499
Jerome Jutteau <jerome.jutteau@outscale.com>
2016-02-26 10:26:52 +0100

    switch: fix uninitialized error
    Signed-off-by: Jerome Jutteau <jerome.jutteau@outscale.com>

--- switch/tests/tests.c
+++ switch/tests/tests.c
@@ -1270,7 +1270,7 @@ static void test_switch(void)
 
 int main(int argc, char **argv)
 {
-	struct pg_error *error;
+	struct pg_error *error = NULL;
 	int r;
 
 	/* tests in the same order as the header function declarations */
