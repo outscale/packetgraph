d4ad2aea307e8e3052a989d8274a51587281b77b
Jerome Jutteau <jerome.jutteau@outscale.com>
2016-05-27 15:11:35 +0200

    vtep: fix public header documentation
    Signed-off-by: Jerome Jutteau <jerome.jutteau@outscale.com>

--- include/packetgraph/vtep.h
+++ include/packetgraph/vtep.h
@@ -69,9 +69,10 @@ void pg_vtep_add_vni(struct pg_brick *brick,
  * @mac:	the vtep mac
  * @flag:	the vtep flag options(see pg_vtep_flags)
  * @errp:	an error pointer
+ * @return	a pointer to a brick structure on success, NULL on error
  */
 struct pg_brick *pg_vtep_new(const char *name, uint32_t west_max,
-		       uint32_t east_max, enum pg_side output,
-		       uint32_t ip, struct ether_addr mac,
-		       int flag, struct pg_error **errp);
+			     uint32_t east_max, enum pg_side output,
+			     uint32_t ip, struct ether_addr mac,
+			     int flag, struct pg_error **errp);
 #endif /* _PG_VTEP_H */
