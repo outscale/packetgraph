28ff63138cfd99c413df497d745fc4067e7de2e0
Tarrade Nicolas <nicolas.tarrade@outscale.com>
2016-09-08 10:51:24 +0200

    vhost: change into monopole
    Signed-off-by: Tarrade Nicolas <nicolas.tarrade@outscale.com>

--- tests/vhost/bench-vhost.c
+++ tests/vhost/bench-vhost.c
@@ -64,9 +64,9 @@ void test_benchmark_vhost(char *vm_image_path,
 	g_assert(ret == 0);
 	g_assert(!error);
 
-	vhost_enter = pg_vhost_new("vhost-enter", 1, 1, EAST_SIDE, &error);
+	vhost_enter = pg_vhost_new("vhost-enter", &error);
 	g_assert(!error);
-	vhost_exit = pg_vhost_new("vhost-exit", 1, 1, EAST_SIDE, &error);
+	vhost_exit = pg_vhost_new("vhost-exit", &error);
 	g_assert(!error);
 
 	socket_path_enter = pg_vhost_socket_path(vhost_enter, &error);
