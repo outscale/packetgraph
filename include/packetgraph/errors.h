1882910bdd0274763e8c9f473c82dcb952f12860
Matthias Gatto <matthias.gatto@outscale.com>
2017-02-01 11:51:33 +0100

    error: add pg_error_prepend
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- include/packetgraph/errors.h
+++ include/packetgraph/errors.h
@@ -65,6 +65,32 @@ struct pg_error {
 
 #define pg_error_new(...) pg_error_new_errno(0, __VA_ARGS__)
 
+#define PG_SUB_ERROR_FMT			\
+	"\n\t-- Error: file='%s' function='%s' line=%lu\n\t   %s"
+
+#define pg_error_prepend(error, format, ...)				\
+	pg_error_prepend_(error, __FILE__,  __LINE__, __func__,		\
+			  format PG_SUB_ERROR_FMT, __VA_ARGS__)
+
+#define pg_error_prepend_(error, file_, line_, function_, format, ...)	\
+	do {								\
+		char *file_tmp = (char *)(error)->context.file;		\
+		char *func_tmp = (char *)(error)->context.function;	\
+		char *msg_tmp = (error)->message;			\
+		(error)->context.file = g_strdup(file_);		\
+		(error)->context.has_line = 1;				\
+		(error)->context.line = line_;				\
+		(error)->context.function = g_strdup(function_);	\
+		(error)->message = g_strdup_printf(format, __VA_ARGS__,	\
+						   file_tmp, func_tmp,	\
+						   (error)->context.line, \
+						   msg_tmp);		\
+		g_free(file_tmp);					\
+		g_free(func_tmp);					\
+		g_free(msg_tmp);					\
+	} while (0)
+
+
 PG_WARN_UNUSED
 struct pg_error *__pg_error_new(int err_no, const char *file,
 				uint64_t line, const char *function,
