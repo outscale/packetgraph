e1a4288fcb6c13894bdf2ebdee603d20b0900fc6
Jerome Jutteau <jerome.jutteau@outscale.com>
2017-03-13 15:21:38 +0100

    misc: packetgraph is not butterfly
    This commit change project name in all license headers.
    Some time ago, packetgraph was part of Butterfly, now it's an
    independant library.
    
    Signed-off-by: Jerome Jutteau <jerome.jutteau@outscale.com>

--- include/packetgraph/lifecycle.h
+++ include/packetgraph/lifecycle.h
@@ -1,18 +1,18 @@
 /* Copyright 2014 Nodalink EURL
  *
- * This file is part of Butterfly.
+ * This file is part of Packetgraph.
  *
- * Butterfly is free software: you can redistribute it and/or modify
+ * Packetgraph is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published
  * by the Free Software Foundation.
  *
- * Butterfly is distributed in the hope that it will be useful,
+ * Packetgraph is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Butterfly.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Packetgraph.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _PG_LIFECYCLE_H
