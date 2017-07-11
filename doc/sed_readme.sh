9b23952ffa5a216a3a9e61b2dcb2affc8ff69e7a
Thiery Ouattara <thiery.ouattara@outscale.com>
2017-07-11 15:26:24 +0200

    doc: update script for documentation
    Signed-off-by: Thiery Ouattara <thiery.ouattara@outscale.com>

--- doc/sed_readme.sh
+++ doc/sed_readme.sh
@@ -3,7 +3,7 @@
 dir=$(cd "$(dirname $0)/.." && pwd)
 
 if [ -e $dir/README.md ]; then
-    sed -e 's/\[!\[Build Status\](https:\/\/travis-ci.org\/outscale\/packetgraph.svg?branch=master)\](https:\/\/travis-ci.org\/outscale\/packetgraph)/\\htmlonly \<a href=\"https:\/\/travis-ci.org\/outscale\/packetgraph\"\> \<img src=\"https:\/\/travis-ci.org\/outscale\/packetgraph.svg?branch=master\"\> \<\/a\>\\endhtmlonly/g' $dir/README.md > $dir/doc/README2.md
+    sed -e 's/\[!\[Build Status\](https:\/\/travis-ci.org\/outscale\/packetgraph.svg?branch=master)\](https:\/\/travis-ci.org\/outscale\/packetgraph) \[!\[packetgraph documentation\](doc\/packetgraph_doc.png)\](https:\/\/outscale.github.io\/packetgraph\/doc\/master)/\\htmlonly \<a href=\"https:\/\/travis-ci.org\/outscale\/packetgraph\"\> \<img src=\"https:\/\/travis-ci.org\/outscale\/packetgraph.svg?branch=master\"\> \<\/a\>\\endhtmlonly/g' $dir/README.md > $dir/doc/README2.md
     sed -i 's/#betterfly/\\#betterfly/g' $dir/doc/README2.md
     exit 0
 else
