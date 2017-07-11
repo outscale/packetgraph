314cab83ff02fef82071cb5660866a956a5c84b8
Thiery Ouattara <thiery.ouattara@outscale.com>
2017-07-11 15:26:24 +0200

    doc: add script and update Doxyfile for documentation
    Signed-off-by: Thiery Ouattara <thiery.ouattara@outscale.com>

---
+++ doc/check_error.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+dir=$(cd "$(dirname $0)" && pwd)
+
+if [ -s $dir/doxygen_error.txt ]; then
+	cat $dir/doxygen_error.txt
+	rm  $dir/doxygen_error.txt
+	exit 1
+fi
+
+rm  $dir/doxygen_error.txt || { echo " Missing $dir/doxygen_error.txt file"; exit 1; }
+exit 0
