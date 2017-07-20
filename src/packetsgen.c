976ec587a2d31d38c8cd1283218d38489334ef6f
Matthias Gatto <matthias.gatto@outscale.com>
2017-07-20 18:14:56 +0200

    packetsgen: g_assert packet after rte_pktmbuf_clone
    So if rte_pktmbuf_clone fail, it won't segfault anymore
    
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- src/packetsgen.c
+++ src/packetsgen.c
@@ -76,6 +76,7 @@ static int packetsgen_poll(struct pg_brick *brick,
 	pkts = g_new0(struct rte_mbuf*, state->packets_nb);
 	for (i = 0; i < state->packets_nb; i++) {
 		pkts[i] = rte_pktmbuf_clone(state->packets[i], mp);
+		g_assert(pkts[i]);
 		pkts[i]->udata64 = i;
 	}
 
