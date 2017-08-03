074ab915bbc6ad9eadf2eb6c524a56f04e36580f
Thiery Ouattara <thiery.ouattara@outscale.com>
2017-08-03 16:43:39 +0200

    doc: update script for deployment
    Signed-off-by: Thiery Ouattara <thiery.ouattara@outscale.com>

--- doc/deploy_documentation.sh
+++ doc/deploy_documentation.sh
@@ -2,16 +2,16 @@
 
 dir=$(cd "$(dirname $0)" && pwd)
 
+if [ "${TRAVIS_PULL_REQUEST}" != "false" -o "${TRAVIS_BRANCH}" != "master" ]; then
+    echo "Just build..."
+    exit 0;
+fi
+
 # Get the current GitHub pages branch
 git clone https://github.com/outscale/outscale.io-packetgraph.git hosting
 # Clean hosting existing contents
 rm -fr hosting/packetgraph || exit 0
 cd hosting
-
-if [ "${TRAVIS_PULL_REQUEST}" = "false" -o "${TRAVIS_BRANCH}" != "master" ]; then
-    echo "Just build..."
-	exit 0;
-fi
 mkdir -p packetgraph/doc/master
 echo '<!DOCTYPE html><html><head><meta http-equiv="refresh" content="1; URL=https://github.com/outscale" /></head><body></body></html>' > index.html
 echo '<!DOCTYPE html><html><head><meta http-equiv="refresh" content="1; URL=https://github.com/outscale/packetgraph" /></head><body></body></html>' > packetgraph/index.html
@@ -23,12 +23,11 @@ if [ -d "$dir/html" ] && [ -s "$dir/html/index.html" ]; then
     # Add everything in this directory (the Doxygen code documentation)
     git add -A .
     # Commit the added files with a title and description containing the Travis CI
-    git commit -m "Deploy code docs to GitHub Pages.
-
-
-     Travis build info:
-     branch name ${TRAVIS_BRANCH}
-     build number${TRAVIS_BUILD_NUMBER}"
+    git commit -m "Deploy code docs to GitHub Pages\
+    \
+    Travis build info:\
+    branch name ${TRAVIS_BRANCH}\
+    build number${TRAVIS_BUILD_NUMBER}"
     # Force push to the remote master branch.
     git push --force "https://${GH_PACKETGRAPH_TOKEN}@${GH_REPO_REF}" master:master
 else
