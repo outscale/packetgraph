3ed3626b95497d9197f2dc6c722b8264b636cd84
Matthias Gatto <matthias.gatto@outscale.com>
2017-11-10 11:53:53 +0100

    vtep: fix ubsan warning
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- src/vtep.c
+++ src/vtep.c
@@ -186,7 +186,8 @@ static void multicast4_internal(struct vtep_state *state,
 		hdr->ipv4.dst_addr = multicast_ip;
 		break;
 	case IGMP_UNSUBSCRIBE:
-		hdr->ipv4.dst_addr = rte_cpu_to_be_32(IPv4(224, 0, 0, 2));
+		hdr->ipv4.dst_addr =
+			rte_cpu_to_be_32(IPv4(224U, 0, 0, 2));
 		break;
 	default:
 		*errp = pg_error_new("action not handle");
