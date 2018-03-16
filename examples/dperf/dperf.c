8bbc5d7c71aab8e2f2558d7517b65ad66d0c690a
Matthias Gatto <matthias.gatto@outscale.com>
2018-03-16 15:34:04 +0000

    examples: improve dperf
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- examples/dperf/dperf.c
+++ examples/dperf/dperf.c
@@ -41,6 +41,12 @@
 #define DATA_LEN 1000
 #define ether_addr_copy(dst, src) memcpy((dst), (src), ETH_ALEN)
 
+uint64_t is_server;
+uint64_t tot_packets;
+uint64_t tot_packets_in_s;
+uint64_t tot_byte;
+uint64_t tot_byte_in_s;
+
 struct packet {
 	struct ethhdr eth;
 	struct iphdr ip;
@@ -50,6 +56,30 @@ struct packet {
 
 #define PKT_LEN sizeof(struct packet)
 
+static void SignalHandler(int signum)
+{
+	printf("signal recive, dperf will now end\n");
+	if (is_server)
+		printf("total recive %lu packets, Mbits %lu\n",
+		       tot_packets, (tot_byte * 8) / 1024 / 1024);
+	pg_stop();
+	exit(0);
+}
+
+static void rx_callback(struct pg_brick *brick,
+			pg_packet_t **rx_burst,
+			uint16_t rx_burst_len,
+			void *private_data)
+{
+	if (!is_server)
+		return;
+	tot_packets += rx_burst_len;
+	tot_packets_in_s += rx_burst_len;
+	for (; rx_burst_len; ++rx_burst, --rx_burst_len) {
+		tot_byte += pg_packet_len(*rx_burst);
+		tot_byte_in_s += pg_packet_len(*rx_burst);
+	}
+}
 static void tx_callback(struct pg_brick *brick,
 			pg_packet_t **tx_burst,
 			uint16_t *tx_burst_len,
@@ -57,6 +87,14 @@ static void tx_callback(struct pg_brick *brick,
 {
 	static bool copied;
 
+	/*
+	 * in server mode, only send first batch of packets so
+	 * switch and stuff learn about us
+	 */
+	if (is_server && copied) {
+		*tx_burst_len = 0;
+		return;
+	}
 	if (!copied) {
 		for (int i = 0; i < PG_RXTX_MAX_TX_BURST_LEN; i++) {
 			memcpy(pg_packet_data(tx_burst[i]),
@@ -90,6 +128,10 @@ static void check_help(int argc, char **argv)
 		printf("  --sip IP      packet src ip address\n");
 		printf("  --dip IP      packet dst ip address\n");
 		printf("  --ether TYPE  overwrite ethertype (default:0x0800\n");
+		printf("  --time TIME	%s",
+		       "run for TIME seconds then stop dperf\n");
+		printf("  --server      use as server, %s",
+		       "print how many packets have been send, each second\n");
 		printf("  --verbose     print packets summary\n");
 		printf("  --help, -h    show this help\n");
 		printf("\n");
@@ -100,6 +142,10 @@ int main(int argc, char **argv)
 {
 	struct pg_error *error = NULL;
 	int ret;
+	int timer = 0;
+	time_t current_time = time(NULL);
+	time_t end = 0;
+	int check_time = 0;
 	struct pg_brick *nic, *burster, *print;
 	struct pg_graph *graph;
 	static struct packet pkt;
@@ -117,7 +163,7 @@ int main(int argc, char **argv)
 
 	nic = pg_nic_new_by_id("port 0", 0, &error);
 	CHECK_ERROR(error);
-	burster = pg_rxtx_new("tx", NULL, &tx_callback, (void *) &pkt);
+	burster = pg_rxtx_new("tx", &rx_callback, &tx_callback, (void *) &pkt);
 
 	pg_nic_get_mac(nic, (struct ether_addr *) &pkt.eth.h_source);
 	tmp_mac = ether_aton("ff:ff:ff:ff:ff:ff");
@@ -138,6 +184,7 @@ int main(int argc, char **argv)
 
 	argc -= ret + 1;
 	argv += ret + 1;
+	ret = 1;
 	while (argc > 0) {
 		if (g_str_equal(argv[0], "--sip")) {
 			if (argc < 2) {
@@ -206,8 +253,19 @@ int main(int argc, char **argv)
 			pkt.eth.h_proto = htons(ether_tmp);
 			--argc;
 			++argv;
+		} else if (g_str_equal(argv[0], "--time")) {
+			if (argc < 2) {
+				fprintf(stderr, "--time needs argument\n");
+				goto exit;
+			}
+			timer = 1;
+			end = current_time + atoi(argv[1]);
+			--argc;
+			++argv;
 		} else if (g_str_equal(argv[0], "--verbose")) {
 			verbose = true;
+		} else if (g_str_equal(argv[0], "--server")) {
+			is_server = true;
 		} else {
 			fprintf(stderr, "unkown argument %s\n", argv[0]);
 			goto exit;
@@ -215,7 +273,6 @@ int main(int argc, char **argv)
 		--argc;
 		++argv;
 	}
-
 	pkt.ip.check = pg_packet_ipv4_checksum((uint8_t *) &pkt.ip);
 	if (verbose) {
 		print = pg_print_new("printer", NULL,
@@ -230,14 +287,37 @@ int main(int argc, char **argv)
 	CHECK_ERROR(error);
 	graph = pg_graph_new("dperf", nic, &error);
 	CHECK_ERROR(error);
+	signal(SIGINT, SignalHandler);
+	signal(SIGQUIT, SignalHandler);
+	signal(SIGSTOP, SignalHandler);
+	signal(SIGTERM, SignalHandler);
 	printf("let's burst packets !\n");
 	while (42) {
 		if (pg_graph_poll(graph, &error) < 0) {
 			CHECK_ERROR(error);
 			break;
 		}
+		if ((timer || is_server) && check_time & 15) {
+			time_t new_time = time(NULL);
+
+			if (is_server && new_time != current_time) {
+				printf("recive %lu packets,Mbits %lu\n",
+				       tot_packets_in_s,
+				       (tot_byte_in_s * 8) / 1024 / 1024);
+				tot_packets_in_s = 0;
+				tot_byte_in_s = 0;
+			}
+			current_time = new_time;
+			if (timer && current_time == end)
+				break;
+		}
+		++check_time;
 	}
+	ret = 0;
+	if (is_server)
+		printf("total recive %lu packets,Mbits %lu\n",
+		       tot_packets, (tot_byte * 8) / 1024 / 1024);
 exit:
 	pg_stop();
-	return 1;
+	return ret;
 }
