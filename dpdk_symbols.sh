63c213eba38b1b17c5166416465eab8c00844f5d
Matthias Gatto <matthias.gatto@outscale.com>
2018-12-03 14:30:10 +0100

    build: fix dpdk_symbols.sh with some odd names
    on my arch chroot, I got PG_DPDK_INIT call generate
    with function containing dot in the name
    example: "tailqinitfn_rte_vfio_tailq.cold.3"
    
    So I've patch the grep so it exclude every function containing a dot
    
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- dpdk_symbols.sh
+++ dpdk_symbols.sh
@@ -10,7 +10,7 @@ tmp=pg_tmp
 for a in ${RTE_SDK}/build/lib/librte_*.a ; do
     if [ ! "$a" == "librte_dpdk.a" ]; then
         # the last 'cut' is here to void grep to generate an error on empty output
-        nm $a | grep -e initfn_ -e mp_hdlr_init_ | cut -f 3 -d " " | grep -v "^\." | cut -f 1 >> $tmp
+        nm $a | grep -e initfn_ -e mp_hdlr_init_ | cut -f 3 -d " " | grep -v "\." | cut -f 1 >> $tmp
     fi
 done
 
