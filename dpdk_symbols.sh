83e2841d99a6ead8c9d7a04770d48be853fc4226
Axel Viau <axel.viau@outscale.com>
2019-03-28 14:37:13 +0000

    build: RTE_SDK removed
    Signed-off-by: Axel Viau <axel.viau@outscale.com>

--- dpdk_symbols.sh
+++ dpdk_symbols.sh
@@ -3,18 +3,18 @@ set -e
 
 source error_print.sh
 
-[ -z "$RTE_SDK" ] && error_print "RTE_SDK path is not set" && exit 1
+[ ! -d "./src/dpdk" ] && error_print "dpdk not found" && exit 1
 
 tmp=pg_tmp
 
-for a in ${RTE_SDK}/build/lib/librte_*.a ; do
+for a in ./src/dpdk/build/lib/librte_*.a ; do
     if [ ! "$a" == "librte_dpdk.a" ]; then
         # the last 'cut' is here to void grep to generate an error on empty output
         nm "$a" | grep -e initfn_ -e mp_hdlr_init_ | cut -f 3 -d " " | grep -v "\." | cut -f 1 >> $tmp
     fi
 done
 
-for a in ${RTE_SDK}/build/lib/librte_*.a ; do
+for a in ./src/dpdk/build/lib/librte_*.a ; do
     if [ ! "$a" == "librte_dpdk.a" ]; then
         objcopy --globalize-symbols=$tmp "$a"
     fi
