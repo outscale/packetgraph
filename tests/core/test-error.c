d23ab4289a1ec869a58434ac5dc178a2cc0289f7
Francois NOUAILLE <francois.nouaille-degorce@outscale.com>
2019-09-09 06:33:09 +0000

    tests: add some unit tests for errors.
    Signed-off-by: Francois NOUAILLE <francois.nouaille-degorce@outscale.com>

--- tests/core/test-error.c
+++ tests/core/test-error.c
@@ -15,10 +15,14 @@
  * along with Packetgraph.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdio.h>
+#include <memory.h>
+#include <unistd.h>
+
 #include <errno.h>
 #include <glib.h>
 
-#include <packetgraph/errors.h>
+#include "utils/errors.h"
 #include "utils/tests.h"
 #include "tests.h"
 
@@ -112,10 +116,76 @@ static void test_error_format(void)
 	pg_error_free(error);
 }
 
+static void test_error_make_ctx_internal(void)
+{
+	struct pg_error_context *
+	ec = pg_error_make_ctx(0);
+	unsigned int line_or_error = __LINE__ - 1;
+
+	g_assert(ec->has_line == 1);
+	g_assert(!strcmp(ec->file, "tests/core/test-error.c"));
+	g_assert(ec->line == line_or_error);
+	g_assert(!strcmp(ec->function, "test_error_make_ctx_internal"));
+}
+static void test_error_print(void)
+{
+	/* This buffer will store the expected message. */
+	char expected_output[240];
+
+	/* Create an error for testing. */
+	struct pg_error *
+	err = pg_error_new(0);
+	int line_or_error = __LINE__ - 1;
+
+	/* Prepare the redirection of stderr. */
+	char buffer[BUFSIZ] = {0};
+	int out_pipe[2];
+	int saved_stderr;
+
+	/* Save current output to recover later. */
+	saved_stderr = dup(STDERR_FILENO);
+	/* Redirect stderr to buffer. */
+	pipe(out_pipe);
+	dup2(out_pipe[1], STDERR_FILENO);
+	close(out_pipe[1]);
+
+	/* Test pg_error_print. */
+	pg_error_print(err);
+
+	/* Read the output of pg_error_print. */
+	fflush(stdout);
+	read(out_pipe[0], buffer, BUFSIZ);
+	/* Check the result. */
+	sprintf(expected_output, "Error:  file='tests/core/test-error.c'"
+		   " function='%s'"
+		   " line=%d errno=0:\n\t(null)", __func__, line_or_error);
+	g_assert(!strcmp(expected_output, buffer));
+	/* Free the error. */
+	pg_error_free(err);
+	/* Clear the buffer */
+	memset (buffer, 0, BUFSIZ);
+
+	/* Test pg_error_print with NULL. */
+	pg_error_print(NULL);
+
+	/* Read the output of pg_error_print. */
+	fflush(stdout);
+	read(out_pipe[0], buffer, BUFSIZ);
+	/* Check the result. */
+	g_assert(!strcmp("Error is NULL\n", buffer));
+	pg_error_free(NULL);
+	/* Redirect stderr to the terminal. */
+	dup2(saved_stderr, STDERR_FILENO);
+	close(saved_stderr);
+}
+
 void test_error(void)
 {
-	pg_test_add_func("/error/lifecycle", test_error_lifecycle);
-	pg_test_add_func("/error/vanilla", test_error_vanilla);
-	pg_test_add_func("/error/errno", test_error_errno);
-	pg_test_add_func("/error/format", test_error_format);
+	pg_test_add_func("/core/error/lifecycle", test_error_lifecycle);
+	pg_test_add_func("/core/error/vanilla", test_error_vanilla);
+	pg_test_add_func("/core/error/errno", test_error_errno);
+	pg_test_add_func("/core/error/format", test_error_format);
+	pg_test_add_func("/core/error/error_make_ctx_internal",
+			 test_error_make_ctx_internal);
+	pg_test_add_func("/core/error/error_print", test_error_print);
 }
