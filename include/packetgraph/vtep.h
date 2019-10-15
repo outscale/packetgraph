8dfbe99f3c179dbb106d85a4e487fa69706fa552
Matthias Gatto <matthias.gatto@outscale.com>
2019-10-15 14:28:37 +0200

    vtep: fox doxygen documentation
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- include/packetgraph/vtep.h
+++ include/packetgraph/vtep.h
@@ -51,7 +51,7 @@ struct ether_addr *pg_vtep_get_mac(const struct pg_brick *brick);
  * This operation iterate over every mac
  * It's slow, even with an empty mac table
  * @param brick a pointer to a vtep brick
- * @port_id the port id (so the index of the connected brick)
+ * @param port_id the port id (so the index of the connected brick)
  */
 void pg_vtep4_cleanup_mac(struct pg_brick *brick, int port_id);
 
@@ -60,7 +60,7 @@ void pg_vtep4_cleanup_mac(struct pg_brick *brick, int port_id);
  * This operation iterate over every mac
  * It's slow, even with an empty mac table
  * @param brick a pointer to a vtep brick
- * @port_id the port id (so the index of the connected brick)
+ * @param port_id the port id (so the index of the connected brick)
  */
 void pg_vtep6_cleanup_mac(struct pg_brick *brick, int port_id);
 
