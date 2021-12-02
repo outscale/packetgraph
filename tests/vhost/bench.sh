c915ebba63746d01e7d83fbfc54179f33165f374
Jérôme Jutteau <jerome.jutteau@outscale.com>
2021-12-02 14:15:41 +0100

    tests: update qemu image url
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- tests/vhost/bench.sh
+++ tests/vhost/bench.sh
@@ -51,9 +51,9 @@ if [ ! -d /mnt/huge ]; then
 fi
 
 # Get VM image & key
-IMG_URL=https://osu.eu-west-2.outscale.com/jerome.jutteau/16d1bc0517de5c95aa076a0584b43af6/arch-260417.qcow
+IMG_URL=https://oos.eu-west-2.outscale.com/butterfly/arch-260417.qcow
 IMG_MD5=7bd77480631145b679bc8fac3583b163
-KEY_URL=https://osu.eu-west-2.outscale.com/jerome.jutteau/16d1bc0517de5c95aa076a0584b43af6/arch-260417.rsa
+KEY_URL=https://oos.eu-west-2.outscale.com/butterfly/arch-260417.rsa
 KEY_MD5=eb3d700f2ee166e0dbe00f4e0aa2cef9
 download $IMG_URL $IMG_MD5 vm.qcow
 download $KEY_URL $KEY_MD5 vm.rsa
